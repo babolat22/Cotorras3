ee6c5916fc148e87410ddb571efcf887aa854457
Juanjo <43281331+babolat22@users.noreply.github.com>
2023-07-26 19:17:37 -0300

    Update Comida.java

--- src/cotorras/Comida.java
+++ src/cotorras/Comida.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cotorras;
 
 /**
@@ -11,6 +6,8 @@ package cotorras;
  */
 public  class Comida {
     static String nombre="Alpiste";
-
-   
+    // QUE FEO LO TUYO
+    public Comida(){
+        // ESTO ES NUEVO
+    }
 }
