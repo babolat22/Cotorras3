733bdcec9948ef1a16d11af5a8494ce44f63fe69
Juanjo <43281331+babolat22@users.noreply.github.com>
2023-08-10 17:32:24 -0300

    Modificada AVES desde Netbeans

--- src/cotorras/Aves.java
+++ src/cotorras/Aves.java
@@ -1,12 +1,13 @@
 package cotorras;
 
 public class Aves{
-    char tipo='A';
-    int nros;
+    char tipo='B';
+    int Nros;
     String canto="PiPi piripiriiii......!!";
     
     public String Cantico(){
         System.out.println("El ave hizo.."+ canto);
+        System.out.println("Cambios nuevos");
         return canto;
     
     }
